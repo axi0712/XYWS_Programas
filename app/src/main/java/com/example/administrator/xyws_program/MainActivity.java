f817ba33563dd3d3678e65e0cc7dcdfc88708dfd
13389809 <13389809@qq.com>
2017-06-08 19:44:35 +0800

    Main页面加了个呵呵呵呵的注释

--- app/src/main/java/com/example/administrator/xyws_program/MainActivity.java
+++ app/src/main/java/com/example/administrator/xyws_program/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //hehehe
+        //呵呵呵呵呵
     }
 }
