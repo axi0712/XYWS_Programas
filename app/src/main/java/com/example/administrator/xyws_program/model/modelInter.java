17efcd6ea3b7fcda6e67a2d2b2c2ae22c8a1f0dc
axi0712 <1255645340@qq.com>
2017-06-10 08:47:53 +0800

    hsudg

--- app/src/main/java/com/example/administrator/xyws_program/model/modelInter.java
+++ app/src/main/java/com/example/administrator/xyws_program/model/modelInter.java
@@ -9,7 +9,7 @@ import java.util.Map;
  * 项目名称: 血压卫士
  * 类描述:
  * 创建人: XI
- * 创建时间: 2017/6/9 0009 23:16
+ * 创建时间: 2017/6/10 0010 8:44
  * 修改人:
  * 修改内容:
  * 修改时间:
@@ -40,5 +40,5 @@ import java.util.Map;
 
 
 public interface ModelInter {
-    void get(Map<String,String> map,  MyCallBack callBack);
+    void get(Map<String,String> map, MyCallBack callBack);
 }
